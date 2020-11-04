cc8f85391f0c2b34ccf8b8579d8e8ae474801a27
PappAdam0 <papp.adaam@gmail.com>
2020-11-04 14:47:59 +0100

    Add project files.

---
+++ kockaPoker/Dobas.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kockaPoker
+{
+    class Dobas
+    {
+        int[] kockak = new int[5];
+        public Dobas(int k1, int k2, int k3, int k4, int k5)
+        {
+            kockak[0] = k1;
+            kockak[1] = k2;
+            kockak[2] = k3;
+            kockak[3] = k4;
+            kockak[4] = k5;
+
+        }
+
+        public void EgyDobas()
+        {
+            Random vel = new Random();
+
+            for (int i = 0; i < kockak.Length; i++)
+            {
+                kockak[i] = vel.Next(1,6);
+            }
+            
+        }
+
+        public void Kiiras()
+        {
+            foreach (var i in kockak)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        public string Erteke()
+        {
+            Dictionary<int, int> eredmenyek = new Dictionary<int, int>();
+            string r = " ";
+            for (int i  = 0; i <= 6; i ++)
+            {
+                eredmenyek.Add(i, 0);
+            }
+            foreach (var i in kockak)
+            {
+                eredmenyek[i]++;
+            }
+
+            if (eredmenyek.ContainsValue(5))
+            {
+                r=  "Nagypóker";
+            }
+            else if (eredmenyek.ContainsValue(4))
+            {
+                r= "Póker";
+            }
+            else if (eredmenyek.ContainsValue(3))
+            {
+                r= "Drill";
+            }
+            else if (eredmenyek.ContainsValue(2))
+            {
+                r= "Pár";
+            }
+            return r;
+        }
+           
+    }
+}
---
+++ kockaPoker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kockaPoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dobas d = new Dobas(1,1,1,3,4);
+            //d.EgyDobas();
+            d.Kiiras();
+            d.Erteke();
+            Console.WriteLine(d.Erteke());
+
+            Console.ReadKey();
+        }
+    }
+}
